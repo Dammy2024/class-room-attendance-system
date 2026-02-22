5e061ee9d2167eef2c98b082fd76399611b0b8d3
Oke Damilola <Oke39096@gmail.com>
2026-02-22 03:12:50 +0100

    m

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/attendance-system/'
+  base: '/class-room-attendance-system/attendance-system/'
 })
